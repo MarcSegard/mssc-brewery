304bab82464539bbf182d55d02e5fd7b997d508e
Marc <segardmarc@gmail.com>
2023-05-25 08:47:13 +0200

    Whole development of Customer CRUD

--- src/main/java/guru/springframework/msscbrewery/services/CustomerServiceImpl.java
+++ src/main/java/guru/springframework/msscbrewery/services/CustomerServiceImpl.java
@@ -1,11 +1,13 @@
 package guru.springframework.msscbrewery.services;
 
 import guru.springframework.msscbrewery.web.model.CustomerDto;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
 @Service
+@Slf4j
 public class CustomerServiceImpl implements CustomerService{
 
     @Override
@@ -13,5 +15,24 @@ public class CustomerServiceImpl implements CustomerService{
         return CustomerDto.builder().id(UUID.randomUUID())
                 .name("SEGARD Marc")
                 .build();
+    }
+
+    @Override
+    public CustomerDto saveNewCustomer(CustomerDto customerDto) {
+        return CustomerDto.builder()
+                .id(UUID.randomUUID())
+                .name("SEGARD Marc")
+                .build();
+    }
+
+    @Override
+    public void updateCustomer(UUID customerId, CustomerDto customerDto) {
+        //Todo impl - would add a real impl to update customer
+    }
+
+    @Override
+    public void deleteById(UUID customerId) {
+        //Todo impl - would add a real impl to delete customer
+        log.debug("Deleting a beer...");
     }
 }
